0cce2bbf143953e48700bc21002d9c7474f9ef48
jasoben <jasoben@gmail.com>
2021-04-22 17:11:29 -0400

    messing around with view shifting rotation

---
+++ Assets/Scripts/Controls/PlayerMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerMover : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand;
+    public float moveCoefficient;
+
+    Vector2 direction;
+    Vector3 remappedDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Actions.default_Move.GetChanged(hand))
+        {
+            direction = SteamVR_Actions.default_Move.GetAxis(hand);
+
+            remappedDirection.x = direction.x;
+            remappedDirection.z = direction.y;
+
+            gameObject.transform.Translate(remappedDirection * moveCoefficient);
+        }
+    }
+}
---
+++ Assets/Scripts/Controls/ShowHead.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ShowHead : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand;
+
+    public GameObject headModel, viewer;
+
+    Quaternion headRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Actions.default_ReorientBody.GetStateDown(hand))
+        {
+            headModel.SetActive(true);
+        }
+        if (SteamVR_Actions.default_ReorientBody.GetStateUp(hand))
+        {
+            headRotation = Quaternion.Euler(headModel.transform.rotation.eulerAngles);
+            Debug.Log(headRotation.eulerAngles);
+            viewer.transform.rotation = headRotation;
+            headModel.SetActive(false);
+        }
+        
+    }
+}
